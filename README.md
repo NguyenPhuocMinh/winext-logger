b1225b915dbe4ae0f04a074212c8cb8a516e9172
Minh Nguyen <xiuquay55@gmail.com>
2021-05-26 18:03:33 +0700

    change logger

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 ### Installation
 
-Install the dependencies and devDependencies and start the server.
-
 ```sh
-$ npm install winrow-logger
-```
-
-### How to use
-
-```js
-const logger = require('winrow-logger');
-
-logger.info('message', {requestId : `${requestId}`})
-logger.debug('message', {requestId : `${requestId}`})
-logger.warn('message', {requestId : `${requestId}`})
-logger.error('message', {requestId : `${requestId}`})
+$ npm install winext-logger
 ```
--- config/options.js
+++ config/options.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const config = {
-  levels: {
-    error: 0,
-    debug: 1,
-    warn: 2,
-    data: 3,
-    info: 4,
-  },
-  colors: {
-    error: 'bold red blackBG',
-    debug: 'bold blue blackBG',
-    warn: 'bold yellow blackBG',
-    data: 'bold magenta blackBG',
-    info: 'bold green blackBG',
+module.exports = {
+  winston: {
+    levels: {
+      error: 0,
+      debug: 1,
+      warn: 2,
+      data: 3,
+      info: 4,
+    },
+    colors: {
+      error: 'bold red',
+      debug: 'bold blue',
+      warn: 'bold yellow',
+      data: 'italic magenta',
+      info: 'bold green',
+    }
   }
 };
-
-module.exports = config;
