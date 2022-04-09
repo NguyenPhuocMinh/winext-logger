83252c68848e7b0a9a5f0dfbcf1cc8fb50731b6d
Minh Nguyen <xiuquay55@gmail.com>
2022-04-09 22:35:36 +0700

    modify code

--- config/options.js
+++ config/options.js
@@ -7,28 +7,29 @@ module.exports = {
       debug: 1,
       warn: 2,
       data: 3,
-      info: 4
+      info: 4,
     },
     colors: {
       error: 'bold red',
       debug: 'bold blue',
       warn: 'bold yellow',
       data: 'italic magenta',
-      info: 'bold green'
-    }
+      info: 'bold green',
+    },
   },
   log4js: {
     appenders: {
-      out: { type: 'stdout' }
+      out: { type: 'stdout' },
     },
     categories: {
       default: {
         appenders: ['out'],
-        level: 'debug'
-      }
-    }
+        level: 'debug',
+      },
+    },
   },
   levels: {
-    info: '\x1B[32m\x1B[1mINFO\x1B[22m\x1B[39m'
-  }
+    info: '\x1B[32m\x1B[1mINFO\x1B[22m\x1B[39m',
+    warn: '\x1B[33m\x1B[1mWARN\x1B[22m\x1B[39m',
+  },
 };
